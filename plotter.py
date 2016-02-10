790015cb19b131080e3f620ddfa863784b8bb096
Caleb Jones <self@calebjones.net>
2016-02-09 19:02:25 -0500

    Guard execution of main code inside __main__ block

--- plotter.py
+++ plotter.py
@@ -5,24 +5,11 @@ import cv2
 import sys
 import itertools
 
-img = cv2.imread(sys.argv[1])
-
-height, width = img.shape[0], img.shape[1]
-
-delay = 0.005
+DELAY = 0.005
 
 xPins = [18, 23, 24, 25]
 yPins = [22, 27, 17, 4]
 zPins = [8, 7]
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-# Set all of the GPIO pins to output mode
-for pin in itertools.chain(xPins, yPins, zPins):
-  GPIO.setup(pin, GPIO.OUT)  
-  GPIO.output(pin, False)
-
 
 def moveAxis(pins):
   def move(*inputs):
@@ -39,12 +26,12 @@ NEGATIVE_PATTERN = [(1, 0, 1, 0), (1, 0, 0, 1), (0, 1, 0, 1), (0, 1, 1, 0)]
 POSITIVE_PATTERN = [(0, 1, 1, 0), (0, 1, 0, 1), (1, 0, 0, 1), (1, 0, 1, 0)]
 
 
-def movePattern(mover,  pattern):
+def movePattern(mover, pattern):
   def motion(steps):
     for _ in range(steps):
       for w1, w2, w3, w4 in pattern:
         mover(w1, w2, w3, w4)
-        time.sleep(delay)
+        time.sleep(DELAY)
   return motion
 
 moveLeft = movePattern(moveX, NEGATIVE_PATTERN)
@@ -64,21 +51,34 @@ def drawDot():
   time.sleep(0.5)
 
 
-for x in range(width):
-  for y in range(height):
-    print "Row: {}\nCol: {}".format(x, y)
-    percentDone = (x * y) / (width * height)
-    print "Percent complete: {}%".format(percentDone)
-    red = img[y, x, 2]
-    green = img[y, x, 1]
-    blue = img[y, x, 0]
+if __name__ == '__main__':
+  img = cv2.imread(sys.argv[1])
 
-    if red != 255 and green != 255 and blue != 255:
-      drawDot()
-    moveRight(1)
+  height, width = img.shape[0], img.shape[1]
 
-  #Go back to the start of the row
-  moveLeft(height)
-  moveYUp(1)
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setwarnings(False)
 
-moveYDown(width)
+  # Set all of the GPIO pins to output mode
+  for pin in itertools.chain(xPins, yPins, zPins):
+    GPIO.setup(pin, GPIO.OUT)  
+    GPIO.output(pin, False)
+
+  for x in range(width):
+    for y in range(height):
+      print "Row: {}\nCol: {}".format(x, y)
+      percentDone = (x * y) / (width * height)
+      print "Percent complete: {}%".format(percentDone)
+      red = img[y, x, 2]
+      green = img[y, x, 1]
+      blue = img[y, x, 0]
+
+      if red != 255 and green != 255 and blue != 255:
+        drawDot()
+      moveRight(1)
+
+    #Go back to the start of the row
+    moveLeft(height)
+    moveYUp(1)
+
+  moveYDown(width)
